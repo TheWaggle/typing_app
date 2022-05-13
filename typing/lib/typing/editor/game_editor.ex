888686075e589271a7fabcdc50c85eb88a8bd085
suzuki-t0505 <takumi.s@thewaggle.co.jp>
2022-05-13 15:59:47 +0900

    exercise-2-5

--- typing/lib/typing/editor/game_editor.ex
+++ typing/lib/typing/editor/game_editor.ex
@@ -1,4 +1,6 @@
 defmodule Typing.Editor.GameEditor do
+  import Typing.Utils.KeysDecision
+
   defstruct input_char: "",
             display_char: "",
             char_count: 0,
@@ -29,18 +31,14 @@ defmodule Typing.Editor.GameEditor do
     ArrowDown
   ) ++ [" "]
 
-  def update(%__MODULE__{} = editor, "input_key", %{"key" => key})
-      when key not in @exclusion_key do
-    if(String.at(editor.display_char, editor.now_char_count) == key) do
-      cond do
-        editor.now_char_count == editor.char_count - 1 ->
-          %{editor | display_char: "クリア", input_char: editor.input_char <> key}
+  def update(%__MODULE__{display_char: char, now_char_count: count} = editor, "input_key", %{"key" => key})
+      when key not in @exclusion_key and key_check(char, count, key) do
+    cond do
+      editor.now_char_count == editor.char_count - 1 ->
+        %{editor | display_char: "クリア", input_char: editor.input_char <> key}
 
-        true ->
-          %{editor | input_char: editor.input_char <> key, now_char_count: editor.now_char_count + 1}
-      end
-    else
-      editor
+      true ->
+        %{editor | input_char: editor.input_char <> key, now_char_count: editor.now_char_count + 1}
     end
   end
 
---
+++ typing/lib/typing/utils/keys_decision.ex
@@ -0,0 +1,7 @@
+defmodule Typing.Utils.KeysDecision do
+  defmacro key_check(char, count, key)  do
+    quote do
+      binary_part(unquote(char), unquote(count), 1) == unquote(key)
+    end
+  end
+end
