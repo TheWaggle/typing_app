904b6570fdf0cf9e40b0d3417e7d441bc46aff3c
suzuki-t0505 <takumi.s@thewaggle.co.jp>
2022-05-13 12:21:29 +0900

    exercise-2-4

--- typing/lib/typing/editor/game_editor.ex
+++ typing/lib/typing/editor/game_editor.ex
@@ -1,10 +1,13 @@
 defmodule Typing.Editor.GameEditor do
   defstruct input_char: "",
-            display_char: ""
+            display_char: "",
+            char_count: 0,
+            now_char_count: 0
 
   def construct() do
     %__MODULE__{
-      display_char: "HelloWorld!!"
+      display_char: "HelloWorld!!",
+      char_count: String.length("HelloWorld!!")
     }
   end
 
@@ -28,7 +31,17 @@ defmodule Typing.Editor.GameEditor do
 
   def update(%__MODULE__{} = editor, "input_key", %{"key" => key})
       when key not in @exclusion_key do
-    %{editor | input_char: editor.input_char <> key}
+    if(String.at(editor.display_char, editor.now_char_count) == key) do
+      cond do
+        editor.now_char_count == editor.char_count - 1 ->
+          %{editor | display_char: "クリア", input_char: editor.input_char <> key}
+
+        true ->
+          %{editor | input_char: editor.input_char <> key, now_char_count: editor.now_char_count + 1}
+      end
+    else
+      editor
+    end
   end
 
   def update(%__MODULE__{} = editor, "input_key", _params), do: editor
