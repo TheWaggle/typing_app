64ee906f062b4cf7095aa801297162fcb69f5057
suzuki-t0505 <takumi.s@thewaggle.co.jp>
2022-05-16 16:06:10 +0900

    exercise-2-7

--- typing/lib/typing/editor/game_editor.ex
+++ typing/lib/typing/editor/game_editor.ex
@@ -5,12 +5,18 @@ defmodule Typing.Editor.GameEditor do
             display_char: "",
             char_count: 0,
             now_char_count: 0,
-            failure_count: 0
+            failure_count: 0,
+            game_status: 0
+
+    # game_status
+    # 0・・・ゲーム停止
+    # 1・・・ゲーム実行中
 
   def construct() do
     %__MODULE__{
       display_char: "HelloWorld!!",
-      char_count: String.length("HelloWorld!!")
+      char_count: String.length("HelloWorld!!"),
+      game_status: 1
     }
   end
 
@@ -33,10 +39,10 @@ defmodule Typing.Editor.GameEditor do
   ) ++ [" "]
 
   def update(%__MODULE__{display_char: char, now_char_count: count} = editor, "input_key", %{"key" => key})
-      when key not in @exclusion_key and key_check(char, count, key) do
+      when key not in @exclusion_key and key_check(char, count, key) and editor.game_status == 1 do
     cond do
       editor.now_char_count == editor.char_count - 1 ->
-        %{editor | display_char: "クリア", input_char: editor.input_char <> key}
+        %{editor | display_char: "クリア", input_char: editor.input_char <> key, game_status: 0}
 
       true ->
         %{editor | input_char: editor.input_char <> key, now_char_count: editor.now_char_count + 1}
@@ -44,7 +50,7 @@ defmodule Typing.Editor.GameEditor do
   end
 
   def update(%__MODULE__{} = editor, "input_key", %{"key" => key})
-      when key not in @exclusion_key do
+      when key not in @exclusion_key and editor.game_status == 1 do
     %{editor | failure_count: editor.failure_count + 1}
   end
 
