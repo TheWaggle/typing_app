74bb616aad5918ec7efd2689a34b5bc022f7ab4f
suzuki-t0505 <takumi.s@thewaggle.co.jp>
2022-05-17 11:41:04 +0900

    exercise-2-9

--- typing/lib/typing_web/views/game_editor_view.ex
+++ typing/lib/typing_web/views/game_editor_view.ex
@@ -1,3 +1,9 @@
 defmodule TypingWeb.GameEditorView do
   use TypingWeb, :view
+
+  def trem_display_char(editor) when editor.input_char != "" do
+    String.trim(editor.display_char, editor.input_char)
+  end
+
+  def trem_display_char(editor), do: editor.display_char
 end
