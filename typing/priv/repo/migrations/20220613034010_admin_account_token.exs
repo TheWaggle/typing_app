5315531ea5e9e8c3fd7a45d86bdaf2ea4e5c9df5
suzuki-t0505 <takumi.s@thewaggle.co.jp>
2022-06-13 15:25:16 +0900

    add admin_account table

---
+++ typing/priv/repo/migrations/20220613034010_admin_account_token.exs
@@ -0,0 +1,17 @@
+defmodule Typing.Repo.Migrations.AdminAccountToken do
+  use Ecto.Migration
+
+  def change do
+    create table(:admin_account_tokens, primary_key: false) do
+      add(:id, :binary_id, primary_key: false)
+      add(:account_id, references(:admin_accounts, type: :binary_id, on_delete: :delete_all), null: false)
+      add(:token, :binary, null: false)
+      add(:context, :string, null: false)
+
+      timestamps(update_at: false)
+    end
+
+    create index(:admin_account_tokens, [:account_id])
+    create unique_index(:admin_account_tokens, [:token, :context])
+  end
+end
