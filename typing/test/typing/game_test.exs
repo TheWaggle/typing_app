142ea01d26c67769791e9ea4a34f4f3ab0a0a1c2
suzuki-t0505 <takumi.s@thewaggle.co.jp>
2022-06-16 11:36:44 +0900

    seard test get_theme

--- typing/test/typing/game_test.exs
+++ typing/test/typing/game_test.exs
@@ -12,6 +12,17 @@ defmodule Typing.GameTest do
       get_theme = Game.get_theme(create_theme.id)
       assert match?(%Game.Theme{}, get_theme)
     end
+
+    test "登録されていないidの場合はnilを取得します。" do
+      theme = "Enum.map([1, 2, 3], fn a -> a * 2 end)"
+      description = "リストの各要素に対して処理をした結果をリストとして返します。"
+      Factory.insert!(:game_themes, %{theme: theme, description: description})
+
+      id = Ecto.UUID.generate()
+
+      get_theme = Game.get_theme(id)
+      assert match?(nil, get_theme)
+    end
   end
 
   describe "get_themes/0" do
