a2e8e90d87ad7fdde536261d8d72dac6a4cca8a0
suzuki-t0505 <takumi.s@thewaggle.co.jp>
2022-06-17 19:23:29 +0900

    install tailwind

--- typing/config/dev.exs
+++ typing/config/dev.exs
@@ -25,7 +25,8 @@ config :typing, TypingWeb.Endpoint,
   secret_key_base: "sMkt7bFvk9nfxl1aQFarW14SeIrrsJMndMSVLW7EPchly5d0ltHXumd0StwKrwHs",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
