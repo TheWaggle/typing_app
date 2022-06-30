da7484cc687895d67e9688fe53b8f1d9da6156f2
suzuki-t0505 <takumi.s@thewaggle.co.jp>
2022-06-30 11:48:36 +0900

    exercise-7-6

--- typing/lib/typing_web/views/admin_account_editor_view.ex
+++ typing/lib/typing_web/views/admin_account_editor_view.ex
@@ -1,3 +1,11 @@
 defmodule TypingWeb.AdminAccountEditorView do
   use TypingWeb, :view
+
+  def get_authority_status(account) do
+    case account.authority_status do
+      0 -> "閲覧者"
+      1 -> "作業者"
+      2 -> "管理者"
+    end
+  end
 end
