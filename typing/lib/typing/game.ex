93fd5a6b63229fccc8f8ea31166502580afdb161
suzuki-t0505 <takumi.s@thewaggle.co.jp>
2022-07-14 14:24:59 +0900

    exercise-8-9

--- typing/lib/typing/game.ex
+++ typing/lib/typing/game.ex
@@ -7,4 +7,14 @@ defmodule Typing.Game do
   """
   @spec get_themes() :: [Game.Theme.t()] | []
   def get_themes(), do: Repo.all(Game.Theme)
+
+  @doc """
+  game_themeを登録する
+  """
+  @spec create_theme(%{}) :: {:ok, Game.Theme.t()} | {:error, Ecto.Changeset.t()}
+  def create_theme(attrs) do
+    %Game.Theme{}
+    |> Game.Theme.changeset(attrs)
+    |> Repo.insert()
+  end
 end
