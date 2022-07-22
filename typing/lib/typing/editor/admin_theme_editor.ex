5ebb48e89f66d3e8f02a17b874d8a7853ed303ae
suzuki-t0505 <takumi.s@thewaggle.co.jp>
2022-07-22 19:35:32 +0900

    exercise-8-19

--- typing/lib/typing/editor/admin_theme_editor.ex
+++ typing/lib/typing/editor/admin_theme_editor.ex
@@ -24,4 +24,12 @@ defmodule Typing.Editor.AdminThemeEditor do
   def update(%__MODULE__{} = editor, "show", %{"id" => id}) do
     %{editor | theme: Game.get_theme(id), mode: :show}
   end
+
+  def update(%__MODULE__{} = editor, "delete", %{"id" => id}) do
+    id
+    |> Game.get_theme()
+    |> Game.delete_theme()
+
+    %{editor | theme: nil, themes: Game.get_themes(), mode: :summary}
+  end
 end
